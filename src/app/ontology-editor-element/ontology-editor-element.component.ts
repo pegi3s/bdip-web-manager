25723afe524a3ffa860059afe80f858cd8580465
PabloG02 <devpablogg@gmail.com>
2025-03-03 10:32:26 +0100

    Add dialog to create/edit container metadata

--- src/app/ontology-editor-element/ontology-editor-element.component.ts
+++ src/app/ontology-editor-element/ontology-editor-element.component.ts
@@ -2,6 +2,10 @@ import { Component, HostBinding, HostListener, Input, inject, input } from '@ang
 import { TermStanza } from '../obo/TermStanza';
 import { Ontology } from '../obo/Ontology';
 import { SvgIconComponent } from "angular-svg-icon";
+import { ImageMetadataDialogComponent } from "../image-metadata-dialog/image-metadata-dialog.component";
+import { Dialog } from "@angular/cdk/dialog";
+import { ContainerLocalService } from "../services/container-local.service";
+import { ImageMetadata } from "../models/image-metadata";
 
 @Component({
     selector: 'app-ontology-editor-element',
@@ -16,6 +20,9 @@ export class OntologyEditorElementComponent {
   category = input<TermStanza>();
   containers = input<Map<string, Set<string>>>();
 
+  dialog: Dialog = inject(Dialog);
+  containerService: ContainerLocalService = inject(ContainerLocalService);
+
   @HostBinding('class.open') opened = false;
 
   @HostListener('click', ['$event'])
@@ -29,8 +36,47 @@ export class OntologyEditorElementComponent {
     this.opened = !this.opened;
   }
 
-  addStanza() {
-    console.log('Adding stanza...');
+  async createEditContainer(name?: string) {
+    const metadata = name ?
+      await this.containerService.getContainerMetadata(name) :
+      {
+        name: "",
+        description: "",
+        status: "Usable",
+        recommended: [],
+        latest: "",
+        bug_found: [],
+        not_working: [],
+        no_longer_tested: [],
+        manual_url: "",
+        source_url: "",
+        comments: [],
+        gui: false,
+        gui_command: "",
+        podman: "untested",
+        singularity: "untested",
+        invocation_general: "",
+        usual_invocation_specific: "",
+        usual_invocation_specific_comments: [],
+        test_invocation_specific: "",
+        test_data_url: "",
+        test_results_url: "",
+        icon: "",
+        input_data_type: [],
+        auto_tests: []
+      } as ImageMetadata;
+
+    const dialogRef = this.dialog.open(ImageMetadataDialogComponent, {
+      width: '800px',
+      maxHeight: '90vh',
+      data: { metadata }
+    });
+
+    dialogRef.closed.subscribe(result => {
+      if (result) {
+        this.containerService.setContainerMetadata(result as ImageMetadata);
+      }
+    });
   }
 
   removeStanza() {
